ca8e2d531dda375d27c14187bd9ccdf930644402
Rui Ma <535657150@qq.com>
2024-09-16 20:58:34 +0800

    优化日历显示

--- frontend/src/components/operating-system/components/CalendarWidget.vue
+++ frontend/src/components/operating-system/components/CalendarWidget.vue
@@ -3,28 +3,22 @@ import { ref } from 'vue'
 import useTimer from '@/hooks/useTimer'
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
-import axios from '@/utils/request'
 import { getHolidayDetail, type HolidayDetail } from '@/utils/getHolidayDetail'
 
 dayjs.locale('zh-cn')
 
-const props = defineProps({
+defineProps({
   showSeconds: {
     type: Boolean,
     default: false
   }
 })
 
-const currentTime = ref('')
+const currentTime = ref<HolidayDetail>()
 const next7DaysEvent = ref<Array<HolidayDetail>>([])
 
-
 useTimer(async () => {
-  const time = await axios.get<number>('/system/timestamp')
-  currentTime.value = dayjs(time).format(props.showSeconds ? 'M月D日 dddd HH:mm:ss' : 'M月D日 dddd HH:mm')
-}, { interval: props.showSeconds ? 1000 : 1000 * 60 })
-
-useTimer(async () => {
+  currentTime.value = await getHolidayDetail()
   const temp: Array<HolidayDetail> = []
   for (let i = 0; i < 7; i++) {
     await getHolidayDetail(dayjs().hour(24 * (i + 1)).format('YYYY-MM-DD')).then((res) => {
@@ -44,11 +38,17 @@ useTimer(async () => {
     class="w-full h-full select-none bg-white p-4 rounded-lg flex gap-0 flex-nowrap min-w-64 shadow-lg overflow-hidden">
     <div class="flex flex-col flex-1 gap-4">
       <div class="flex items-center gap-0">
-        <div class="text-5xl font-bold">{{ dayjs().format('D') }}</div>
+        <div class="text-5xl font-bold">{{ currentTime?.date.format('D') }}</div>
         <div class="w-[1px] bg-slate-500 h-6 ml-2 mr-1"></div>
-        <div class="writing-vertical-lr select-none font-bold">{{ dayjs().format('ddd') }}</div>
+        <div class="writing-vertical-lr select-none font-bold">{{ currentTime?.date.format('ddd') }}</div>
       </div>
-      <div class="h-full w-full flex items-center justify-start text-xs text-gray-500">无事项</div>
+      <div class="h-full w-full flex items-start justify-start text-xs text-gray-500">
+        <div class="text-xs font-bold text-nowrap flex gap-1 items-center" v-for="(tag, index) in currentTime?.tags"
+             :key="index">
+          <div class="w-[0.3rem] h-4 rounded-full" :class="tag.color"></div>
+          <div>{{ tag.name }}</div>
+        </div>
+      </div>
     </div>
     <div class="flex flex-col gap-2 flex-1">
       <template v-if="next7DaysEvent.length > 0">
